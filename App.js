29e765af516764f97b64e4eb5c0ab202a98b01d7
blaker859 <112280753+blaker859@users.noreply.github.com>
2023-02-01 23:30:01 -0800

    added image background added resizeMode, a ViewStyle, and an ImageStyle for image

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
@@ -7,11 +7,21 @@ import StartGameScreen from "./screens/StartGameScreen";
 export default function App() {
   return (
     <LinearGradient colors={["#ddb52f", "#4e0329"]} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen} //styling the view
+        imageStyle={styles.backGroundImage} //prop associated with ImageBackground it is a combination of a view and a image prop //styling the actual image
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: { flex: 1 },
+  backGroundImage: {
+    opacity: 0.14,
+  },
 });
