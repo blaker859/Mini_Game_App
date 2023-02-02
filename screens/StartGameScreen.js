cd507f62a9000ed490fc131cb435c40717162dc5
blaker859 <112280753+blaker859@users.noreply.github.com>
2023-02-01 22:05:26 -0800

    imported stylesheet to startscreen created stylesheet and added styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,9 @@
-import { TextInput, Pressable, View } from "react-native";
+import { TextInput, Pressable, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -11,3 +11,11 @@ function StartGameScreen() {
   );
 }
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    marginTop: 100,
+    padding: 16,
+  },
+});
