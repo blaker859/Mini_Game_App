173588f0df6d7041748e555a22ef1c9b2154b860
blaker859 <112280753+blaker859@users.noreply.github.com>
2023-02-01 23:20:17 -0800

    importing LinearGradient from expo npm installing to project and adding colors prop to LinearGradient Container which takes an array of 2 colors

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient colors={["#ddb52f", "#4e0329"]} style={styles.rootScreen}>
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  rootScreen: { backgroundColor: "#ddb52f", flex: 1 },
+  rootScreen: { flex: 1 },
 });
