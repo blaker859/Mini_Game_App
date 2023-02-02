0f576c391c1a0d3747739b0b6f9e493f9cbf4f4d
blaker859 <112280753+blaker859@users.noreply.github.com>
2023-02-01 22:15:02 -0800

    added shadows per platform ios takes 4 properties color, offset, radius, opacity

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -14,8 +14,16 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
+    flex: 0,
+    marginHorizontal: 24,
+    borderRadius: 8, //rounds corners
     marginTop: 100,
     padding: 16,
+    backgroundColor: "#72063c",
+    elevation: 4, //adding a shadow for android
+    shadowColor: "black", //needed for shadow on ios
+    shadowOffset: { width: 10, height: 10 }, //needed for shadow on ios
+    shadowRadius: 6, //needed for shadow on ios
+    shadowOpacity: 1, //needed for shadow on ios
   },
 });
