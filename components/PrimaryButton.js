74f7eb1d910ec866049e7e9ff93b86fcf96b3991
blaker859 <112280753+blaker859@users.noreply.github.com>
2023-02-01 21:40:39 -0800

    importing Primary button to Start screen and using it for reset and confirm

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,2 +1,20 @@
-function PrimaryButton() {}
+import { View, Text } from "react-native";
+
+// accepts props
+// function PrimaryButton(props) {
+//   return (
+//     <View>
+//       <Text>{props.children}</Text>
+//     </View>
+//   );
+// }
+
+// destructuring props.children so we don't have to access props everytime we reference it
+function PrimaryButton({ children }) {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+}
 export default PrimaryButton;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,12 @@
 import { TextInput, Pressable, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return;
   <View>
     <TextInput />
-    {/* buttons */}
+    <PrimaryButton>Reset</PrimaryButton>
+    <PrimaryButton>Confirm</PrimaryButton>
   </View>;
 }
 export default StartGameScreen;
